f67729dad7b560306c066a23a187b7006c13ff23
quandtifier <quandtifier@gmail.com>
2018-04-04 11:31:15 -0700

    Initial commit

---
+++ app/src/main/java/edu/tacoma/uw/css/quandm/dialogslab/DialogsActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.quandm.dialogslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DialogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+    }
+}
