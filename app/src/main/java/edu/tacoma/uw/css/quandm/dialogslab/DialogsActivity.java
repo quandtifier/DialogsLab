44d614d150a72366a6729e00bce6dd27ce0d725a
quandtifier <quandtifier@gmail.com>
2018-04-04 13:04:54 -0700

    Working through step 7 Had to implement OnFragmentInteractionListener from DialogsActivity

--- app/src/main/java/edu/tacoma/uw/css/quandm/dialogslab/DialogsActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/quandm/dialogslab/DialogsActivity.java
@@ -1,13 +1,34 @@
 package edu.tacoma.uw.css.quandm.dialogslab;
 
+import android.net.Uri;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class DialogsActivity extends AppCompatActivity {
+public class DialogsActivity extends AppCompatActivity
+        implements FireMissilesDialogFragment.OnFragmentInteractionListener{
 
+    public static final String TAG = "DialogsActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialogs);
+    }
+
+    public void launch(View view) {
+        DialogFragment fragment = null;
+        if (view.getId() == R.id.btn_fire_missiles) {
+            fragment = new FireMissilesDialogFragment();
+        } if (fragment != null) {
+            fragment.show(getSupportFragmentManager(), "launch");
+        }
+        Log.v(TAG, "end of launch");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
